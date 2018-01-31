6b5c9126c74985b16b534e9a73fede7c0e0c593c
php 17 <php.17@dino.cec.ime.usp.br>
2018-01-30 22:34:08 -0200

    detalhes de front end

--- resources/views/saudacao.blade.php
+++ resources/views/saudacao.blade.php
@@ -3,10 +3,13 @@
   <head>
     <meta charset="utf-8">
     <title>Saudação</title>
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
   </head>
   <body>
+    <div class="container">
     @for($i = 0; $i < 100; $i++)
     <h1>Olá {{ $i }}</h1>
     @endfor
+    <script type="text/javascript" src="/js/app.js">
   </body>
 </html>
